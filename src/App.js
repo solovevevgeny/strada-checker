0c31989e5e8c6389861ec2557ad8d36c761f7636
Solovev Evgeny <evgeny.solovev@gmail.com>
2024-07-26 14:37:24 +0300

    add events

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ function App() {
         <Checker url="https://heroleague.ru/api/event/event_format/lastrada2024_spb1" name="Велогонка"/>
         <Checker url="https://heroleague.ru/api/event/event_format/lastrada2024_spb2" name="Фестиваль"/>
         <Checker url="https://heroleague.ru/api/event/event_format/poseidon2024_spb" name="Poseidon"/>
+        <Checker url="https://heroleague.ru/api/event/event_format/urban2024_spb" name="Гонка героев URBAN"/>
+        <Checker url="https://heroleague.ru/api/event/event_format/trail2024_498817_2707" name="Trail"/>
       </div>
 
     </div>
