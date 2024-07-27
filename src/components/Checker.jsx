b0fd854ed5e2b2b83e5d0562882d1d0da5a656ff
solovev.evgeny <evgeny.solovev@gmail.com>
2024-07-27 22:18:40 +0300

    logo for events

--- src/components/Checker.jsx
+++ src/components/Checker.jsx
@@ -22,24 +22,44 @@ function Checker(props) {
         <div className="container list">
             {
                 checkerState.map ((item) => {
+
+                    let logo = item.event_type.logo;
+
+                    if (!logo.includes('https://')) {
+                        logo = "https://heroleague.ru" + logo;
+                    }
+
                     return (
                             <div className="card shadow">
-                                <div className="card-body">
-                                    <div class="card-body__city">
-                                        {item.city.name_ru}
+                                <div className="card-body" >
+
+                                    <div className="card-body__left">
+                                        <div class="card-body__city">
+                                            {item.city.name_ru}
+                                        </div>
+                                        <div class="card-body__type">
+                                            {item.title}
+                                        </div>
+                                        <div class="card-body__price">
+                                            {item.price} &#8381;
+                                        </div>
+                                        <div class="card-body__count-title">
+                                            Осталось слотов:
+                                        </div>
+                                        <div class="card-body__count">
+                                            {(item.tickets_left.athlete === -1) ? 0 : item.tickets_left.athlete}
+                                        </div>
                                     </div>
-                                    <div class="card-body__type">
-                                        {item.title}
+
+                                    <div className="card-body__right">
+                                        <div className="card-body__logo">
+                                            <img src={logo} alt="" width="100%"/>
+                                        </div>
                                     </div>
-                                    <div class="card-body__price">
-                                        {item.price} &#8381;
-                                    </div>
-                                    <div class="card-body__count-title">
-                                        Осталось слотов:
-                                    </div>
-                                    <div class="card-body__count">
-                                        {(item.tickets_left.athlete === -1) ? 0 : item.tickets_left.athlete}
-                                    </div>
+
+
+
+
                                 </div>
                             </div>
                     )
