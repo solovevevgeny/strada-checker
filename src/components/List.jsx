99d90faf4c2958f4201f5bd15ab40fac6656f077
solovev.evgeny <evgeny.solovev@gmail.com>
2024-07-28 10:51:20 +0300

    events list

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -11,7 +11,7 @@ export const List = () => {
                 <Checker url="https://heroleague.ru/api/event/event_format/poseidon2024_spb" name="Poseidon"/>
                 <Checker url="https://heroleague.ru/api/event/event_format/urban2024_spb" name="Гонка героев URBAN"/>
                 {/* <Checker url="https://heroleague.ru/api/event/event_format/trail2024_498817_2707" name="Trail"/> */}
-                <Checker url="https://heroleague.ru/api/event/event_format/lastrada2024_spb1" name="Велогонка"/>
+                {/* <Checker url="https://heroleague.ru/api/event/event_format/lastrada2024_spb1" name="Велогонка"/> */}
         </>
     )
     
