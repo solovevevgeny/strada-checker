583158b18cac34b70941b94532940afdec0cd1d0
Solovev Evgeny <evgeny.solovev@gmail.com>
2024-07-30 12:36:13 +0300

    events list

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -5,14 +5,14 @@ export const List = () => {
 
     return (
         <>
-                <Checker url="https://heroleague.ru/api/event/event_format/lastrada2024_spb1" name="Велогонка"/>
-                <Checker url="https://heroleague.ru/api/event/event_format/lastrada2024_spb2" name="Фестиваль"/>
-                <Checker url="https://heroleague.ru/api/event/event_format/lastrada2024_spb3" name="LaStrada OFFROAD"/>
-                <Checker url="https://heroleague.ru/api/event/event_format/lastrada2024_sochi1" name="Велогонка СОЧИ"/>
+                <Checker url="https://heroleague.ru/api/event/event_format/lastrada2024_spb1" name="Велогонка 29 сентября 2024"/>
+                <Checker url="https://heroleague.ru/api/event/event_format/lastrada2024_spb2" name="Фестиваль 29 сентября 2024"/>
+                <Checker url="https://heroleague.ru/api/event/event_format/lastrada2024_spb3" name="LaStrada OFFROAD 7 сентября 2024"/>
+                <Checker url="https://heroleague.ru/api/event/event_format/lastrada2024_sochi1" name="Велогонка СОЧИ 29 сентября 2024"/>
 
-                <Checker url="https://heroleague.ru/api/event/event_format/poseidon2024_spb" name="Poseidon"/>
-                <Checker url="https://heroleague.ru/api/event/event_format/urban2024_spb" name="Гонка героев URBAN"/>
-                <Checker url="https://heroleague.ru/api/event/event_format/eforum2024_2013348" name="ЗАБЕГ #МЫБЕРЕЖЕМ"/>
+                <Checker url="https://heroleague.ru/api/event/event_format/poseidon2024_spb" name="Poseidon 17 августа 2024"/>
+                <Checker url="https://heroleague.ru/api/event/event_format/urban2024_spb" name="Гонка героев URBAN 31 августа 2024"/>
+                <Checker url="https://heroleague.ru/api/event/event_format/eforum2024_2013348" name="ЗАБЕГ #МЫБЕРЕЖЕМ 4 сентября 2024"/>
                 {/* <Checker url="https://heroleague.ru/api/event/event_format/trail2024_498817_2707" name="Trail"/> */}
                 {/* <Checker url="https://heroleague.ru/api/event/event_format/lastrada2024_spb1" name="Велогонка"/> */}
         </>
