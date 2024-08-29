aa097a0516a14e1a75452344e747f56e1fbb2d61
Solovev Evgeny <evgeny.solovev@gmail.com>
2024-08-29 14:51:32 +0300

    list update

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -9,14 +9,16 @@ export const List = () => {
         <>
 
                 <Info text="Перенос гонки с 11 августа на 29 сентября!" link="https://t.me/lastrada_official/907" />
+                <Checker url="https://heroleague.ru/api/event/event_format/urban2024_spb" name="Гонка героев URBAN 31 августа 2024"/>
+                <Checker url="https://heroleague.ru/api/event/event_format/lastrada2024_spb3" name="LaStrada OFFROAD 7 сентября 2024"/>
+
                 <Checker url="https://heroleague.ru/api/event/event_format/lastrada2024_spb1" name="Велогонка 29 сентября 2024"/>
                 <Checker url="https://heroleague.ru/api/event/event_format/lastrada2024_spb2" name="Фестиваль 29 сентября 2024"/>
-                <Checker url="https://heroleague.ru/api/event/event_format/lastrada2024_spb3" name="LaStrada OFFROAD 7 сентября 2024"/>
                 <Checker url="https://heroleague.ru/api/event/event_format/lastrada2024_sochi1" name="Велогонка СОЧИ 29 сентября 2024"/>
+                
 
-                <Checker url="https://heroleague.ru/api/event/event_format/poseidon2024_spb" name="Poseidon 17 августа 2024"/>
-                <Checker url="https://heroleague.ru/api/event/event_format/urban2024_spb" name="Гонка героев URBAN 31 августа 2024"/>
-                <Checker url="https://heroleague.ru/api/event/event_format/eforum2024_2013348" name="ЗАБЕГ #МЫБЕРЕЖЕМ 4 сентября 2024"/>
+                {/* <Checker url="https://heroleague.ru/api/event/event_format/poseidon2024_spb" name="Poseidon 17 августа 2024"/> */}
+                {/* <Checker url="https://heroleague.ru/api/event/event_format/eforum2024_2013348" name="ЗАБЕГ #МЫБЕРЕЖЕМ 4 сентября 2024"/> */}
                 {/* <Checker url="https://heroleague.ru/api/event/event_format/trail2024_498817_2707" name="Trail"/> */}
                 {/* <Checker url="https://heroleague.ru/api/event/event_format/lastrada2024_spb1" name="Велогонка"/> */}
         </>
