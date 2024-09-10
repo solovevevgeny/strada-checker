cbd8f000425c6560b4dc196709e93fb8a9908a0d
Solovev Evgeny <evgeny.solovev@gmail.com>
2024-09-10 09:58:25 +0300

    list

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -12,11 +12,14 @@ export const List = () => {
                 <Info text="требования к оборудованию и снаряжению для участия в велогонке/велофестивале/оффроуд?" link="https://faq.heroleague.ru/knowledge-bases/4/articles/23896-kakie-trebovaniya-k-oborudovaniyu-i-snaryazheniyu-dlya-uchastiya-v-velogonkevelofestivaleoffroud" />
                 
                 {/* <Checker url="https://heroleague.ru/api/event/event_format/urban2024_spb" name="Гонка героев URBAN 31 августа 2024"/> */}
-                <Checker url="https://heroleague.ru/api/event/event_format/lastrada2024_spb3" name="LaStrada OFFROAD 7 сентября 2024"/>
-                <Checker url="https://heroleague.ru/api/event/event_format/velogonka2024" name="ДВЕ СТОЛИЦЫ 14 сентября 2024"/>
+                {/* <Checker url="https://heroleague.ru/api/event/event_format/lastrada2024_spb3" name="LaStrada OFFROAD 7 сентября 2024"/> */}
+                {/* <Checker url="https://heroleague.ru/api/event/event_format/velogonka2024" name="ДВЕ СТОЛИЦЫ 14 сентября 2024"/> */}
 
+
+                <Checker url="https://heroleague.ru/api/event/event_format/gonka2024_524901_1409" name="ГОНКА ГЕРОЕВ 14 СЕНТЯБРЯ"/>
 
                 <Checker url="https://heroleague.ru/api/event/event_format/lastrada2024_spb1" name="Велогонка 29 сентября 2024"/>
+
                 <Checker url="https://heroleague.ru/api/event/event_format/lastrada2024_spb2" name="Фестиваль 29 сентября 2024"/>
                 <Checker url="https://heroleague.ru/api/event/event_format/lastrada2024_sochi1" name="Велогонка СОЧИ 29 сентября 2024"/>
 
