9a08f16a0d9398a27ecc5b42edac5265578e1b6e
Solovev Evgeny <evgeny.solovev@gmail.com>
2024-12-11 12:20:09 +0300

    list

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -9,18 +9,15 @@ export const List = () => {
         <>
 
                 {/* <Info text="Какая справка требуется для участия в велогонке?" link="https://faq.heroleague.ru/knowledge-bases/4/articles/23904-kakaya-spravka-trebuetsya-dlya-uchastiya-v-velogonke" /> */}
-                <Checker url="https://heroleague.ru/api/event/event_format/lastrada2024offroad_msc_1011" name="LA STRADA - ОФФ-РОУД 10 ноября 2024"/>
-                <Checker url="https://heroleague.ru/api/event/event_format/zima2025_524901_1502" name="ГОНКА ГЕРОЕВ ЗИМА 15 ФЕВРАЛЯ"/>
-
-                <Checker url="https://heroleague.ru/api/event/event_format/zima2025_ufa_2202" name="ГОНКА ГЕРОЕВ ЗИМА 22 ФЕВРАЛЯ"/>
-
-                <Checker url="https://heroleague.ru/api/event/event_format/gonka2025_498817_2308" name="ГОНКА ГЕРОЕВ ЗИМА 23 Августа, Песочненский"/>
-
 
                 
-                {/* <Checker url="https://heroleague.ru/api/event/event_format/trail2024_524901_0510" name="Ночной ТРЕЙЛ 5 октября"/> */}
-                {/* <Checker url="https://heroleague.ru/api/event/event_format/trail2024_524901_0610" name="Дневной ТРЕЙЛ 6 октября"/> */}
-
+                <Checker url="https://heroleague.ru/api/event/event_format/doroga2025" name="Дорога Жизни 26 января"/>
+                <Checker url="https://heroleague.ru/api/event/event_format/zabeg2025_498817" name="Забег.РФ 24 мая"/>
+                <Checker url="https://heroleague.ru/api/event/event_format/trail2025_498817_2007" name="Trail 20 июля"/>
+                <Checker url="https://heroleague.ru/api/event/event_format/gonka2025_498817_2308" name="Гонка Героев 23 августа"/>
+                <Checker url="https://heroleague.ru/api/event/event_format/urban2025_spb" name="Городские препятствия 11 октября"/>
+          
+         
         </>
     )
     
