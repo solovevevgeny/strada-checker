c9b512fb1b4e9677491a93d3a153d7f3270ae37f
Solovev Evgeny <33507596+solovevevgeny@users.noreply.github.com>
2026-01-16 14:51:45 +0300

    Update List.jsx

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -11,13 +11,8 @@ export const List = () => {
                 {/* <Info text="Какая справка требуется для участия в велогонке?" link="https://faq.heroleague.ru/knowledge-bases/4/articles/23904-kakaya-spravka-trebuetsya-dlya-uchastiya-v-velogonke" /> */}
 
                 
-                <Checker url="https://heroleague.ru/api/event/event_format/doroga2025" name="Дорога Жизни 26 января"/>
-                <Checker url="https://heroleague.ru/api/event/event_format/zabeg2025_498817" name="Забег.РФ 24 мая"/>
-                <Checker url="https://heroleague.ru/api/event/event_format/trail2025_498817_2007" name="Trail 20 июля"/>
-                <Checker url="https://heroleague.ru/api/event/event_format/gonka2025_498817_2308" name="Гонка Героев 23 августа"/>
-                <Checker url="https://heroleague.ru/api/event/event_format/urban2025_spb" name="Городские препятствия 11 октября"/>
-
-                <Checker url="https://heroleague.ru/api/event_format/event/lastrada2024criterium_msc" name="La Strada Критериум"/>
+                <Checker url="https://heroleague.ru/api/event/event_format/doroga2026" name="Дорога Жизни 25 января"/>
+           
          
         </>
     )
