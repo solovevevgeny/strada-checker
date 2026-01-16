ed24829e0f584ef7ca122cacfb1629b28ea8d6c7
Solovev Evgeny <evgeny.solovev@gmail.com>
2026-01-16 14:58:59 +0300

    List

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -11,7 +11,8 @@ export const List = () => {
                 {/* <Info text="Какая справка требуется для участия в велогонке?" link="https://faq.heroleague.ru/knowledge-bases/4/articles/23904-kakaya-spravka-trebuetsya-dlya-uchastiya-v-velogonke" /> */}
 
                 
-                <Checker url="https://heroleague.ru/api/event/event_format/doroga2026" name="Дорога Жизни 25 января"/>
+                <Checker url="https://heroleague.ru/api/event_format/event/doroga2026" name="Дорога Жизни 25 января"/>
+                              
            
          
         </>
